43e705d27cfcbf3503fc2d4ee61076253d6221ae
nour al maleh <nourmale7@gmail.com>
2022-10-13 23:43:00 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    return string.toLowerCase();
+  } 
+
+  whisper(HELLO)
+
+  function logShout(string){
+    console.log(string.toUpperCase());
+  }
+
+  whisper(HELLO)
+
+  function logWhisper(string){
+    console.log(string.toLowerCase());
+  }
+
+
+  function sayHiToHeadphonedRoommate(string) {
+const a = "I can't hear you!"
+const b = "YES INDEED!"
+const c = "I would love to!"
+if (string.toLowerCase(string)=== string ){
+  return a; 
+}
+else if (string.toUpperCase(string) === string ){
+  return b; 
+}
+else if ("Let's have dinner together!" === string ){
+  return c; 
+}
+  }
+
+  
